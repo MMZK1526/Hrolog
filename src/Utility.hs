8f2c994a98f1b7403d11e78a81e98c11b19fff1a
Jiacheng Liang <lengjunyi@outlook.com>
2023-01-23 22:51:13 +0000

    [ljc] Carelessly implemented 3 functions unifyTerm, substituteTerm, substituteAtom

--- src/Utility.hs
+++ src/Utility.hs
@@ -1,16 +1,25 @@
 module Utility where
 
-import           Data.Map (Map)
+import           Data.Map (Map, singleton, empty, lookup)
 import           Program
+import Data.Maybe (fromMaybe)
 
 unifyTerm :: Term -> Term -> Maybe (Map String Term)
-unifyTerm = undefined
+unifyTerm (VariableTerm x) t'@(ConstantTerm _) = Just (singleton x t') 
+unifyTerm t t'@(VariableTerm _) = unifyTerm t' t
+unifyTerm (ConstantTerm c) (ConstantTerm c')
+    | c == c'   = Just empty
+    | otherwise = Nothing
 
 unifyAtom :: Atom -> Atom -> Maybe (Map String Term)
-unifyAtom = undefined
+unifyAtom (Atom p ts) (Atom p' ts')
+    | p /= p'   = Nothing
+    | otherwise = undefined
 
 substituteTerm :: Map String Term -> Term -> Term
-substituteTerm = undefined
+substituteTerm m t@(VariableTerm x) = fromMaybe t (Data.Map.lookup x m)
+substituteTerm _ t                  = t
+
 
 substituteAtom :: Map String Term -> Atom -> Atom
-substituteAtom = undefined
+substituteAtom m (Atom p ts) = Atom p (map (substituteTerm m) ts)
