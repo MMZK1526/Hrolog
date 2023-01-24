81c03d07c1ecd80c90a072490e9a4c4885f3c631
mmzk1526 <yc4120@ic.ac.uk>
2023-01-24 22:31:18 +0000

    [mmzk] (refactor) Use guard

--- src/Utility.hs
+++ src/Utility.hs
@@ -1,31 +1,28 @@
 module Utility where
 
-import           Data.Map (Map, singleton, empty, lookup)
+import           Control.Monad
+import           Data.Map (Map)
+import qualified Data.Map as M
+import           Data.Maybe
 import           Program
-import Data.Maybe (fromMaybe)
 
 unifyTerm :: Term -> Term -> Maybe (Map String Term)
-unifyTerm (VariableTerm x) t'@(ConstantTerm _) = Just (singleton x t') 
-unifyTerm t t'@(VariableTerm _) = unifyTerm t' t
-unifyTerm (ConstantTerm c) (ConstantTerm c')
-  | c == c'   = Just empty
-  | otherwise = Nothing
+unifyTerm (VariableTerm x) t'@(ConstantTerm _) = Just (M.singleton x t') 
+unifyTerm t t'@(VariableTerm _)                = unifyTerm t' t
+unifyTerm (ConstantTerm c) (ConstantTerm c')   = M.empty <$ guard (c == c')
 
 unifyAtom :: Atom -> Atom -> Maybe (Map String Term)
 unifyAtom (Atom p ts) (Atom p' ts')
   | p /= p'   = Nothing
-  | otherwise = foldl addToMap (Just empty) (zip ts ts')
+  | otherwise = foldl addToMap (Just M.empty) (zip ts ts')
   where
     addToMap mm (t, t') = do
       m <- mm
-      let nt  = substituteTerm m t
-      let nt' = substituteTerm m t' 
-      unifyTerm nt nt'
+      unifyTerm (substituteTerm m t) (substituteTerm m t' )
 
 substituteTerm :: Map String Term -> Term -> Term
-substituteTerm m t@(VariableTerm x) = fromMaybe t (Data.Map.lookup x m)
+substituteTerm m t@(VariableTerm x) = fromMaybe t (M.lookup x m)
 substituteTerm _ t                  = t
-
 
 substituteAtom :: Map String Term -> Atom -> Atom
 substituteAtom m (Atom p ts) = Atom p (map (substituteTerm m) ts)
