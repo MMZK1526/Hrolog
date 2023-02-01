cf6996634e413a4abbc575c1b396b6459ed0ad56
mmzk1526 <yc4120@ic.ac.uk>
2023-02-01 00:57:43 +0000

    [mmzk] (refactor) Reformat solve'

--- src/Solver.hs
+++ src/Solver.hs
@@ -1,20 +1,17 @@
 module Solver where
 
-import Data.Map (Map)
+import           Data.Map (Map)
 import qualified Data.Map as M
-import Data.Maybe (catMaybes)
-import Program ( Program(Program), Term, Clause (Clause), Atom )
-import qualified Program as P
-import Utility (unifyAtom)
+import           Data.Maybe
+import           Program
+import           Utility
 
 solve :: Program -> Atom -> [Map String Term]
 solve = solve' M.empty
 
 solve' :: Map String Term -> Program -> Atom -> [Map String Term]
--- solve' = undefined
-solve' _ (Program _ _ cs) t =
-  catMaybes $ do
-    Clause {P._clauseHead = h, P._clauseBody = _} <- cs
-    return $ case h of
-      Nothing -> Nothing
-      Just t' -> (unifyAtom t t')
+solve' _ (Program _ _ cs) t = catMaybes $ do
+  Clause { _clauseHead = h, _clauseBody = _ } <- cs
+  return $ case h of
+    Nothing -> Nothing
+    Just t' -> unifyAtom t t'
