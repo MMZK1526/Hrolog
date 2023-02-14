ebbdf61b5d5a9cab388658132343017c85e51c87
mmzk1526 <yc4120@ic.ac.uk>
2023-02-14 12:28:52 +0000

    [mmzk] (feat) Unfinished skeleton for prolog interactive shell

--- src/Main.hs
+++ src/Main.hs
@@ -1,26 +1,41 @@
 module Main where
 
-import Parser
+import           Control.Monad
+import           Control.Monad.Trans.Class
+import           Control.Monad.Trans.State
+import           Parser
+import           Solver.Prolog
 import Program
-import Solver.Prolog
 import Utility
 
-query :: String
-query = "gt(X, Y), gt(Y, Z)."
+testQuery :: String
+testQuery = "gt(X, Y), gt(Y, Z)."
+
+runProlog :: StateT FilePath IO ()
+runProlog = do
+  path <- get
+  src  <- lift $ readFile path
+  case parseProgram src of
+    Left pErr     -> do
+      lift $ putStrLn "Error parsing the program!"
+      lift $ putStrLn pErr
+    Right program -> do
+      lift $ putStrLn (concat ["Program ", show path, " loaded."])
+      let queryFeedback = forever $ do
+            input <- lift getLine
+            case parsePQuery input of
+              Left qErr   -> do
+                lift $ putStrLn "Error parsing the query!"
+                lift $ putStrLn qErr
+              Right query -> do
+                let subs = head $ solve program query
+                let x    = foldl (flip substituteTerm) (VariableTerm "X") subs
+                let y    = foldl (flip substituteTerm) (VariableTerm "Y") subs
+                let z    = foldl (flip substituteTerm) (VariableTerm "Z") subs
+                lift $ putStrLn $ "The solution for 'X' is " ++ pShow x
+                lift $ putStrLn $ "The solution for 'Y' is " ++ pShow y
+                lift $ putStrLn $ "The solution for 'Z' is " ++ pShow z
+      queryFeedback
 
 main :: IO ()
-main = do
-  src <- readFile "test/programs/simpleNumbers.hrolog"
-  case parseProgram src of
-    Left err  -> putStrLn err
-    Right ast -> do
-      pPrint ast
-      let Right q = parsePQuery query
-      putStrLn $ "Query is " ++ pShow q
-      let subs = head $ solve ast q
-      let x    = foldl (flip substituteTerm) (VariableTerm "X") subs
-      let y    = foldl (flip substituteTerm) (VariableTerm "Y") subs
-      let z    = foldl (flip substituteTerm) (VariableTerm "Z") subs
-      putStrLn $ "The solution for 'X' is " ++ pShow x
-      putStrLn $ "The solution for 'Y' is " ++ pShow y
-      putStrLn $ "The solution for 'Z' is " ++ pShow z
+main = void $ runStateT runProlog "test/programs/simpleNumbers.hrolog"
