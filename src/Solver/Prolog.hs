8323a25b42cdf1883b606d90239d9d5eeb774092
mmzk1526 <yc4120@ic.ac.uk>
2023-02-20 14:39:43 +0000

    [mmzk] (feat) Boilerplate for prolog solver state

--- src/Solver/Prolog.hs
+++ src/Solver/Prolog.hs
@@ -6,28 +6,43 @@ import           Control.Monad
 import           Control.Monad.Trans.State
 import           Data.Map (Map)
 import qualified Data.Map as M
+import           Data.Set (Set)
 import qualified Data.Set as S
 import           Program
 import           Utility
+
+-- | The state used by the Prolog solver.
+--
+-- TODO: Explanation.
+data PState = PState
+  { pStep    :: Int                 -- ^ Number of steps
+  , pProven  :: Set Atom            -- ^ What's been proven so far
+  , pTry     :: Map Atom (Set Atom) -- ^ What we are trying to prove
+  , pLeadsTo :: Map Atom (Set Atom) -- ^ What we can deduce
+  }
+
+emptyState :: PState
+emptyState = PState 1 S.empty M.empty M.empty
 
 -- | Given the "Program" and the Prolog query, return a list of variable
 -- substitutions and all intermediate substitutions, each representing a
 -- solution.
 solve :: Program -> PQuery -> [(Solution, [Map String Term])]
 solve (Program _ _ _ cs) (PQuery vars query)
-  = (\subs -> (findVarSub subs, subs)) <$> evalState (worker (1 :: Int) M.empty query) ()
+  = (\subs -> (findVarSub subs, subs)) <$> evalState (worker M.empty query) emptyState
   where
     subVar subs v   = (v, foldl (flip substituteTerm) (VariableTerm v) subs)
     findVarSub subs = Solution $ M.fromList (subVar subs <$> S.toList vars)
 
-    worker _ sub []          = pure [[sub]]
-    worker step sub (t : ts) = fmap concat . forM cs $ \(mH :?<- b) -> case mH of
+    worker sub []       = pure [[sub]]
+    worker sub (t : ts) = fmap concat . forM cs $ \(mH :?<- b) -> case mH of
       Nothing -> pure []
       Just a' -> do
+        step <- gets pStep
         let rename = renameAtom (show step ++ "#")
         case unifyAtom t (rename a') of
           Nothing   -> pure []
           Just sub' -> do
-            rest <- worker (step + 1) sub'
-                           (substituteAtom sub' <$> (map rename b ++ ts))
+            modify (\s -> s { pStep = step + 1 })
+            rest <- worker sub' (substituteAtom sub' <$> (map rename b ++ ts))
             return $ (sub :) <$> rest
